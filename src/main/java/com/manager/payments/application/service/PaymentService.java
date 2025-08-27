65b1ba67c2950b66443dfdccd85049f84df39f76
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-08-28 00:19:57 +0200

    feat: deleted unused use case

--- src/main/java/com/manager/payments/application/service/PaymentService.java
+++ src/main/java/com/manager/payments/application/service/PaymentService.java
@@ -2,17 +2,13 @@ package com.manager.payments.application.service;
 
 import com.manager.payments.adapter.in.rest.dto.CreatePaymentRequestDTO;
 import com.manager.payments.application.port.in.CreatePaymentUseCase;
-import com.manager.payments.application.port.in.FindPaymentUseCase;
 import com.manager.payments.application.port.out.PaymentRepository;
-import com.manager.payments.model.exceptions.PaymentNotFoundException;
 import com.manager.payments.model.payments.Payment;
 import com.manager.payments.model.payments.PaymentFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
-public class PaymentService implements CreatePaymentUseCase, FindPaymentUseCase {
+public class PaymentService implements CreatePaymentUseCase {
 
     private final PaymentRepository paymentRepository;
 
@@ -25,10 +21,5 @@ public class PaymentService implements CreatePaymentUseCase, FindPaymentUseCase 
         Payment newPayment = PaymentFactory.build(requestDTO.amount(), requestDTO.name(), requestDTO.description(),
                 requestDTO.startDate(), requestDTO.endDate(), requestDTO.periodDays());
         return paymentRepository.save(newPayment);
-    }
-
-    @Override
-    public Payment findById(UUID id) {
-        return paymentRepository.findById(id).orElseThrow(() -> new PaymentNotFoundException(id));
     }
 }
