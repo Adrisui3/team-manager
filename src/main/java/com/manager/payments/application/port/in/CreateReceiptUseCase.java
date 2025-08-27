46cb675ac9710adfaccf2715973952ae9dfe0ef5
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-08-28 00:17:33 +0200

    feat: removed file

--- src/main/java/com/manager/payments/application/port/in/CreateReceiptUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.manager.payments.application.port.in;
-
-import com.manager.payments.model.receipts.ReceiptMinInfo;
-
-import java.util.UUID;
-
-public interface CreateReceiptUseCase {
-    ReceiptMinInfo createReceipt(UUID playerId, UUID paymentId);
-}
