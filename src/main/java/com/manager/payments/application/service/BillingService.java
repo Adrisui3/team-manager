869de1145bb354232a2b215909d0569505b146ce
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-09-07 17:17:48 +0200

    test: adds unit tests

--- src/main/java/com/manager/payments/application/service/BillingService.java
+++ src/main/java/com/manager/payments/application/service/BillingService.java
@@ -3,7 +3,6 @@ package com.manager.payments.application.service;
 import com.manager.payments.application.port.in.IssueNewReceiptsUseCase;
 import com.manager.payments.application.port.out.PaymentRepository;
 import com.manager.payments.application.port.out.PlayerRepository;
-import com.manager.payments.model.exceptions.PaymentNotFoundException;
 import com.manager.payments.model.exceptions.PlayerNotFoundException;
 import com.manager.payments.model.payments.Payment;
 import com.manager.payments.model.receipts.ReceiptMinInfo;
@@ -39,16 +38,14 @@ public class BillingService implements IssueNewReceiptsUseCase {
             paymentRepository.updateNextPaymentDate(payment.id(), newNextPaymentDate);
 
             for (PlayerMinInfo player : payment.players()) {
-                ReceiptMinInfo newReceipt = createReceipt(player.id(), payment.id());
+                ReceiptMinInfo newReceipt = createReceipt(player.id(), payment);
                 logger.info("Created receipt {} for player {}", newReceipt.id(), player.id());
             }
         }
     }
 
-    private ReceiptMinInfo createReceipt(UUID playerId, UUID paymentId) {
+    private ReceiptMinInfo createReceipt(UUID playerId, Payment payment) {
         Player player = playerRepository.findById(playerId).orElseThrow(() -> new PlayerNotFoundException(playerId));
-        Payment payment =
-                paymentRepository.findById(paymentId).orElseThrow(() -> new PaymentNotFoundException(paymentId));
 
         player.createReceiptFor(payment);
         Player updatedPlayer = playerRepository.save(player);
---
+++ src/test/java/com/manager/payments/application/port/in/IssueNewReceiptsUseCaseTest.java
@@ -0,0 +1,62 @@
+package com.manager.payments.application.port.in;
+
+import com.manager.payments.application.port.out.PaymentRepository;
+import com.manager.payments.application.port.out.PlayerRepository;
+import com.manager.payments.application.service.BillingService;
+import com.manager.payments.model.payments.Payment;
+import com.manager.payments.model.payments.PaymentMinInfo;
+import com.manager.payments.model.payments.PaymentStatus;
+import com.manager.payments.model.users.Category;
+import com.manager.payments.model.users.Player;
+import com.manager.payments.model.users.PlayerMinInfo;
+import com.manager.payments.model.users.PlayerStatus;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class IssueNewReceiptsUseCaseTest {
+
+    @Test
+    void shouldCreateNewReceiptForEachPlayer() {
+        // given
+        UUID player1Id = UUID.randomUUID();
+        Player player1 = new Player(player1Id, "", "", "", "", LocalDate.now(), Category.SENIOR,
+                PlayerStatus.ENABLED, new ArrayList<>(), new ArrayList<>());
+        UUID player2Id = UUID.randomUUID();
+        Player player2 = new Player(player2Id, "", "", "", "", LocalDate.now(), Category.SENIOR,
+                PlayerStatus.ENABLED, new ArrayList<>(), new ArrayList<>());
+        UUID paymentId = UUID.randomUUID();
+        Payment payment = new Payment(paymentId, 10, "", "", LocalDate.now(), LocalDate.now(),
+                LocalDate.now(), 10, PaymentStatus.ACTIVE, new ArrayList<>());
+        player1.payments().add(PaymentMinInfo.from(payment));
+        player2.payments().add(PaymentMinInfo.from(payment));
+        payment.players().add(PlayerMinInfo.from(player1));
+        payment.players().add(PlayerMinInfo.from(player2));
+
+        PlayerRepository playerRepository = Mockito.mock(PlayerRepository.class);
+        Mockito.when(playerRepository.findById(player1Id)).thenReturn(Optional.of(player1));
+        Mockito.when(playerRepository.save(player1)).thenReturn(player1);
+        Mockito.when(playerRepository.findById(player2Id)).thenReturn(Optional.of(player2));
+        Mockito.when(playerRepository.save(player2)).thenReturn(player2);
+
+        PaymentRepository paymentRepository = Mockito.mock(PaymentRepository.class);
+        Mockito.when(paymentRepository.findAllActiveAndNextPaymentDateBefore(Mockito.any())).thenReturn(List.of(payment));
+
+        IssueNewReceiptsUseCase issueNewReceiptsUseCase = new BillingService(playerRepository, paymentRepository);
+
+        // when
+        issueNewReceiptsUseCase.issueNewReceipts(LocalDate.now());
+
+        // then
+        assertThat(player1.receipts().size()).isEqualTo(1);
+        assertThat(player2.receipts().size()).isEqualTo(1);
+    }
+
+}
