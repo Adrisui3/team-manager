62af68acfdb018ea07e4464919a8deaa32196b1c
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-09-26 18:30:00 +0200

    fix: makes billing job run 1 hours after marking payments as inactive

--- src/main/java/com/manager/payments/adapter/in/rest/jobs/BillingJob.java
+++ src/main/java/com/manager/payments/adapter/in/rest/jobs/BillingJob.java
@@ -16,7 +16,7 @@ public class BillingJob {
         this.issueNewReceiptsUseCase = issueNewReceiptsUseCase;
     }
 
-    @Scheduled(cron = "0 0 2 * * *")
+    @Scheduled(cron = "0 0 1 * * *")
     @Transactional
     public void billing() {
         LocalDate now = LocalDate.now();
