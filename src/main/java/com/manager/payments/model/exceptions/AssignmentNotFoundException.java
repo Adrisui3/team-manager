f061d44ed17d822542974b878cf6dafc58c8a41f
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-11-15 18:23:47 +0100

    fix: typo

--- src/main/java/com/manager/payments/model/exceptions/AssignmentNotFoundException.java
+++ src/main/java/com/manager/payments/model/exceptions/AssignmentNotFoundException.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 public class AssignmentNotFoundException extends GenericException {
     public AssignmentNotFoundException(UUID playerId, UUID paymentId) {
-        super("No assignment already exists between player " + playerId + " and payment " + paymentId,
+        super("No assignment exists between player " + playerId + " and payment " + paymentId,
                 GenericStatus.INVALID_STATE);
     }
 }
