e940e5c72dc249bf57788a06d2235cd411bb09a4
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-11-16 20:32:01 +0100

    feat: better overall api

--- src/main/java/com/manager/auth/adapter/in/rest/UserController.java
+++ src/main/java/com/manager/auth/adapter/in/rest/UserController.java
@@ -5,14 +5,19 @@ import com.manager.auth.adapter.in.security.AuthenticatedUserProvider;
 import com.manager.auth.adapter.out.persistence.mapper.UserMapper;
 import com.manager.auth.model.users.User;
 import com.manager.shared.response.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Users management endpoints")
 @RestController
-@RequestMapping("users")
+@RequestMapping("/v1/users")
 public class UserController {
 
     private final AuthenticatedUserProvider authenticatedUserProvider;
@@ -23,6 +28,11 @@ public class UserController {
         this.userMapper = userMapper;
     }
 
+    @Operation(summary = "Get authenticated user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Authenticated user",
+                    useReturnTypeSchema = true)
+    })
     @GetMapping("/me")
     public ResponseEntity<ResponseDto<UserDto>> getCurrentUser() {
         User authenticatedUser = authenticatedUserProvider.getAuthenticatedUser();
