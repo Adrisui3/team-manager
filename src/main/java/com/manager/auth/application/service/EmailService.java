2fb57c0ead690de19529c8cc1f5534786dd98157
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-12-12 19:02:38 +0100

    fix: logs exception properly for debugging purposes

--- src/main/java/com/manager/auth/application/service/EmailService.java
+++ src/main/java/com/manager/auth/application/service/EmailService.java
@@ -3,6 +3,8 @@ package com.manager.auth.application.service;
 import com.manager.auth.model.exceptions.VerificationEmailFailedException;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender mailSender;
 
     public EmailService(JavaMailSender mailSender) {
@@ -35,6 +38,7 @@ public class EmailService {
         try {
             sendEmail(to, subject, htmlMessage);
         } catch (MessagingException e) {
+            logger.error("Failed to send verification email to {}", to, e);
             throw new VerificationEmailFailedException("Could no send verification email");
         }
     }
