c15f9a28c7d17098ab8b55b7a50162e285529f52
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-12-20 18:49:03 +0100

    fix: adds validated annotation to jwt config

--- src/main/java/com/manager/auth/application/service/config/JwtConfig.java
+++ src/main/java/com/manager/auth/application/service/config/JwtConfig.java
@@ -2,7 +2,9 @@ package com.manager.auth.application.service.config;
 
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "security.jwt")
 public record JwtConfig(@NotBlank String secretKey, long expirationTime) {
 }
