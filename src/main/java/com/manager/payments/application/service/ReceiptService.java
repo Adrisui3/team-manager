1ff7cc6c92e520d46c504f8b25b411b10b21455c
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-12-29 03:15:04 +0100

    fix: properly sets receipt payment date when its status is updated

--- src/main/java/com/manager/payments/application/service/ReceiptService.java
+++ src/main/java/com/manager/payments/application/service/ReceiptService.java
@@ -1,9 +1,12 @@
 package com.manager.payments.application.service;
 
 import com.manager.payments.application.port.in.ProcessOverdueReceiptsUseCase;
+import com.manager.payments.application.port.in.UpdateReceiptStatusUseCase;
 import com.manager.payments.application.port.out.ReceiptRepository;
+import com.manager.payments.model.exceptions.ReceiptNotFoundException;
 import com.manager.payments.model.receipts.OverdueReceiptProcessor;
 import com.manager.payments.model.receipts.Receipt;
+import com.manager.payments.model.receipts.ReceiptStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,22 +14,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 @Slf4j
-public class ReceiptService implements ProcessOverdueReceiptsUseCase {
+public class ReceiptService implements ProcessOverdueReceiptsUseCase, UpdateReceiptStatusUseCase {
 
-    private final ReceiptRepository receiptRepository;
+    private final ReceiptRepository repository;
 
     @Override
     public void processOverdueReceipts(LocalDate date) {
         log.info("Updating overdue receipts at {}", date);
-        List<Receipt> overdueReceipts = receiptRepository.findAllPendingWithExpirationDateBefore(date);
+        List<Receipt> overdueReceipts = repository.findAllPendingWithExpirationDateBefore(date);
         log.info("Found {} overdue receipts", overdueReceipts.size());
         List<Receipt> processedReceipts = OverdueReceiptProcessor.process(overdueReceipts);
         log.info("Processed {} overdue receipts", overdueReceipts.size());
-        receiptRepository.saveAll(processedReceipts);
+        repository.saveAll(processedReceipts);
+    }
+
+    @Override
+    public Receipt updateStatus(UUID receiptId, ReceiptStatus receiptStatus) {
+        Receipt receipt = repository.findById(receiptId).orElseThrow(() -> new ReceiptNotFoundException(receiptId));
+        LocalDate paymentDate = receiptStatus.equals(ReceiptStatus.PAID) ? LocalDate.now() : null;
+        Receipt updatedReceipt = receipt.toBuilder()
+                .status(receiptStatus)
+                .paymentDate(paymentDate)
+                .build();
+
+        return repository.save(updatedReceipt);
     }
 }
