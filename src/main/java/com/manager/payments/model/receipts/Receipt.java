6451cf0d3022b5f78aba47d1a3b358f85de9d9d9
Adrián José Martínez Navarro <admartnav@gmail.com>
2025-12-29 02:49:52 +0100

    refactor: removes unused methods

--- src/main/java/com/manager/payments/model/receipts/Receipt.java
+++ src/main/java/com/manager/payments/model/receipts/Receipt.java
@@ -5,7 +5,6 @@ import com.manager.payments.model.players.Player;
 import lombok.Builder;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -15,15 +14,4 @@ public record Receipt(UUID id, String code, BigDecimal amount, LocalDate issuedD
                       Player player, Payment payment) {
 
     private static final int RECEIPT_EXPIRATION_DAYS = 15;
-
-    public Receipt(String code, BigDecimal amount, LocalDate issuedDate, LocalDate periodStartDate,
-                   LocalDate periodEndDate, ReceiptStatus status, Player player, Payment payment) {
-        this(null, code, amount.setScale(2, RoundingMode.HALF_UP), issuedDate, null,
-                issuedDate.plusDays(RECEIPT_EXPIRATION_DAYS), periodStartDate, periodEndDate, status, player, payment);
-    }
-
-    public Receipt withStatus(ReceiptStatus status) {
-        return new Receipt(id, code, amount, issuedDate, paymentDate, expiryDate, periodStartDate, periodEndDate,
-                status, player, payment);
-    }
 }
