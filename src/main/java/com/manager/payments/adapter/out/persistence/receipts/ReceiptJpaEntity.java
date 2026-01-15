0380d4e8dd9ded38cbaf19c90f7d5fde2c56db37
Adrián José Martínez Navarro <admartnav@gmail.com>
2026-01-15 20:24:58 +0100

    refactor: adds lombok annotation to jpa entities

--- src/main/java/com/manager/payments/adapter/out/persistence/receipts/ReceiptJpaEntity.java
+++ src/main/java/com/manager/payments/adapter/out/persistence/receipts/ReceiptJpaEntity.java
@@ -4,11 +4,15 @@ import com.manager.payments.adapter.out.persistence.payments.PaymentJpaEntity;
 import com.manager.payments.adapter.out.persistence.players.PlayerJpaEntity;
 import com.manager.payments.model.receipts.ReceiptStatus;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "receipt")
 public class ReceiptJpaEntity {
@@ -42,92 +46,4 @@ public class ReceiptJpaEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "payment_id")
     private PaymentJpaEntity payment;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public LocalDate getIssuedDate() {
-        return issuedDate;
-    }
-
-    public void setIssuedDate(LocalDate issuedDate) {
-        this.issuedDate = issuedDate;
-    }
-
-    public LocalDate getPaymentDate() {
-        return paymentDate;
-    }
-
-    public void setPaymentDate(LocalDate paymentDate) {
-        this.paymentDate = paymentDate;
-    }
-
-    public LocalDate getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(LocalDate expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public LocalDate getPeriodStartDate() {
-        return periodStartDate;
-    }
-
-    public void setPeriodStartDate(LocalDate periodStartDate) {
-        this.periodStartDate = periodStartDate;
-    }
-
-    public LocalDate getPeriodEndDate() {
-        return periodEndDate;
-    }
-
-    public void setPeriodEndDate(LocalDate periodEndDate) {
-        this.periodEndDate = periodEndDate;
-    }
-
-    public ReceiptStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ReceiptStatus status) {
-        this.status = status;
-    }
-
-    public PlayerJpaEntity getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(PlayerJpaEntity player) {
-        this.player = player;
-    }
-
-    public PaymentJpaEntity getPayment() {
-        return payment;
-    }
-
-    public void setPayment(PaymentJpaEntity payment) {
-        this.payment = payment;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
 }
