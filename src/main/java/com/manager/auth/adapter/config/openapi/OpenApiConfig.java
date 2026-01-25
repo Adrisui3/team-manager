6779f6cd1ba016b1233f12d8ea298bfcb0a85be9
Adrián Martínez <admartnav@gmail.com>
2026-01-25 02:20:06 +0100

    fix: adds prod server to open api config (#59)

--- src/main/java/com/manager/auth/adapter/config/openapi/OpenApiConfig.java
+++ src/main/java/com/manager/auth/adapter/config/openapi/OpenApiConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,6 +16,10 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://api.avmonachil.com").description("Production server"),
+                        new Server().url("http://localhost:8080").description("Local development server")
+                ))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
