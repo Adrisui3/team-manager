2269e3f9914bb610eebe671139ebccc2d9cda46b
Adrián José Martínez Navarro <admartnav@gmail.com>
2026-01-31 12:16:31 +0100

    fix: removes unnecessary annotation

--- src/main/java/com/manager/auth/adapter/config/email/ResendConfiguration.java
+++ src/main/java/com/manager/auth/adapter/config/email/ResendConfiguration.java
@@ -2,7 +2,6 @@ package com.manager.auth.adapter.config.email;
 
 import com.resend.Resend;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("prod")
 @RequiredArgsConstructor
-@EnableConfigurationProperties(ResendConfigurationProperties.class)
 public class ResendConfiguration {
 
     private final ResendConfigurationProperties configuration;
