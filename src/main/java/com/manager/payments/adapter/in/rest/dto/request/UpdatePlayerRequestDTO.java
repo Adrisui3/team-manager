ab5844eca6a1a3b760cb6fcc7f09b7fc86a8122f
Adrián José Martínez Navarro <admartnav@gmail.com>
2026-02-11 20:53:20 +0100

    fix: min and max sizes for secondary email

--- src/main/java/com/manager/payments/adapter/in/rest/dto/request/UpdatePlayerRequestDTO.java
+++ src/main/java/com/manager/payments/adapter/in/rest/dto/request/UpdatePlayerRequestDTO.java
@@ -15,8 +15,8 @@ import java.time.LocalDate;
 public record UpdatePlayerRequestDTO(
         @NotBlank @Size(min = 1, max = 60) @Schema(description = "Name") String name,
         @NotBlank @Size(min = 1, max = 80) @Schema(description = "Surname") String surname,
-        @NotBlank @Email @Schema(description = "Email") String email,
-        @Email @Schema(description = "Secondary email") String secondaryEmail,
+        @NotBlank @Size(min = 1, max = 80) @Email @Schema(description = "Email") String email,
+        @Size(min = 1, max = 80) @Email @Schema(description = "Secondary email") String secondaryEmail,
         @NotNull @Schema(description = "Birth date") LocalDate birthDate,
         @NotBlank @Size(min = 9, max = 80) @Schema(description = "Phone number") String phoneNumber,
         @Size(min = 9, max = 80) @Schema(description = "Secondary phone number") String secondaryPhoneNumber,
