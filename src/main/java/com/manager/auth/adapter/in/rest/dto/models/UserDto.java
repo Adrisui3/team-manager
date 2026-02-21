543ffd9b5033d30b1e34fc71220adc1c136778bf
Adrián José Martínez Navarro <admartnav@gmail.com>
2026-02-21 13:04:56 +0100

    fix: adds last login for users

--- src/main/java/com/manager/auth/adapter/in/rest/dto/models/UserDto.java
+++ src/main/java/com/manager/auth/adapter/in/rest/dto/models/UserDto.java
@@ -2,8 +2,10 @@ package com.manager.auth.adapter.in.rest.dto.models;
 
 import com.manager.auth.model.roles.Role;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record UserDto(UUID id, String email, String name, String surname, Role role, boolean enabled) {
+public record UserDto(UUID id, String email, String name, String surname, Role role, boolean enabled,
+                      LocalDateTime lastLogIn) {
 
 }
