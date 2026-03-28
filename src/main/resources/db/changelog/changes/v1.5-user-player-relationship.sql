a162951cda57a26255c1dda653eb0e19e2456e26
Adrián José Martínez Navarro <admartnav@gmail.com>
2026-03-28 20:35:56 +0100

    fix: compatible liquibase changeset

--- src/main/resources/db/changelog/changes/v1.5-user-player-relationship.sql
+++ src/main/resources/db/changelog/changes/v1.5-user-player-relationship.sql
@@ -2,10 +2,9 @@
 --changeset admartnav:0005-user-player-relationship
 
 ALTER TABLE player
-    ADD COLUMN user_id UUID,
-    ADD CONSTRAINT fk_player_user FOREIGN KEY (user_id) REFERENCES users (id) ON
-DELETE
-SET NULL;
+    ADD COLUMN user_id UUID;
+ALTER TABLE player
+    ADD CONSTRAINT fk_player_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL;
 
 --rollback ALTER TABLE player DROP CONSTRAINT fk_player_user;
 --rollback ALTER TABLE player DROP COLUMN user_id;
